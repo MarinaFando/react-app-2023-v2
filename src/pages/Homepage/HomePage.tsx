31aa4b33fc3ce68c5966b1cac002a602224da8f1
Marina Fando <marinafando@gmail.com>
2023-04-02 19:55:12 +0200

    refactor: home page to functional component

--- src/pages/Homepage/HomePage.tsx
+++ src/pages/Homepage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchPanel from '../../components/SearchPanel';
 import { Sneaker } from '../../App';
 import CardList from '../../components/CardList/CardList';
@@ -10,36 +10,28 @@ interface HomePageState {
   searchValue: string;
 }
 
-class HomePage extends React.Component<HomePageProps, HomePageState> {
-  constructor(props: HomePageProps) {
-    super(props);
-    this.state = {
-      searchValue: localStorage.getItem('searchValue') || '',
-    };
-    this.onSearchValueChange = this.onSearchValueChange.bind(this);
+const HomePage = ({items}: HomePageProps) => {
+  const [searchValue, setSearchValue] = useState<string>(localStorage.getItem('searchValue') || '')
+
+  useEffect(() => {
+    return () => {
+      localStorage.setItem('searchValue', searchValue);
+    }
+  }, [searchValue])
+
+  const onSearchValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value)
   }
 
-  onSearchValueChange(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({
-      searchValue: event.target.value,
-    });
-  }
-
-  componentWillUnmount(): void {
-    localStorage.setItem('searchValue', this.state.searchValue);
-  }
-
-  render() {
     return (
       <section>
         <SearchPanel
-          searchValue={this.state.searchValue}
-          onSearchValueChange={this.onSearchValueChange}
+          searchValue={searchValue}
+          onSearchValueChange={onSearchValueChange}
         />
-        <CardList items={this.props.items} searchValue={this.state.searchValue} />
+        <CardList items={items} searchValue={searchValue} />
       </section>
     );
-  }
 }
 
 export default HomePage;
