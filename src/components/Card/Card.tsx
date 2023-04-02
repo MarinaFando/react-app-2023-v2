0af4e127eb6c4944e3111a0169aaffc2727cbd2d
Marina Fando <marinafando@gmail.com>
2023-04-02 19:49:53 +0200

    refactor: card component to functional component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 import { Sneaker } from '../../App';
 import '../../styles/Card.css';
 
-class Card extends React.Component<{ item: Sneaker }> {
-  render() {
+const Card = ({ item }: Sneaker) => {
     return (
       <div className="card">
         <div className="card__favorite">
           <img src="img/liked.svg" alt="Liked item" />
         </div>
-        <img width="133" height="112" src={this.props.item.imageUrl} alt="Sneakers" />
-        <h4>{this.props.item.title}</h4>
+        <img width="133" height="112" src={item.imageUrl} alt="Sneakers" />
+        <h4>{item.title}</h4>
         <div className="card__bottom">
           <div className="card__bottom-price">
             <span>Price:</span>
-            <b>{this.props.item.price}$</b>
+            <b>{item.price}$</b>
           </div>
           <button className="card__bottom-button" type="button">
             Add to cart
@@ -22,7 +21,6 @@ class Card extends React.Component<{ item: Sneaker }> {
         </div>
       </div>
     );
-  }
 }
 
 export default Card;
