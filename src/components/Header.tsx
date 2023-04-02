dc4f0d09811c0a0c36fde5784ee11204340cdb1a
Marina Fando <marinafando@gmail.com>
2023-04-02 19:51:14 +0200

    refactor: card header component to functional component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 
-class Header extends React.Component<object, { path: string }> {
-  constructor() {
-    super();
-    this.state = {
-      path: '...',
-    };
-  }
+const Header = () => {
+  const [path, setPath] = useState('...');
 
-  onLinkClick = (e) => {
-    this.setState({ path: e.target.pathname.slice(1) ? e.target.pathname.slice(1) : 'home' });
+  const onLinkClick = (e) => {
+    setPath( e.target.pathname.slice(1) || 'home');
   };
 
-  render() {
-    return (
-      <header className="header">
-        <Link className="header-link" to="/">
-          <h1>Shopping mall</h1>
+  return (
+    <header className="header">
+      <Link className="header-link" to="/">
+        <h1>Shopping mall</h1>
+      </Link>
+      <h2>{`${path} page`}</h2>
+      <nav className="header-links">
+        <Link className="header-link" to="/" onClick={onLinkClick}>
+          Home
         </Link>
-        <h2>{`${this.state.path} page`}</h2>
-        <nav className="header-links">
-          <Link className="header-link" to="/" onClick={this.onLinkClick}>
-            Home
-          </Link>
-          <Link className="header-link" to="/about" onClick={this.onLinkClick}>
-            About
-          </Link>
-          <Link className="header-link" to="/form" onClick={this.onLinkClick}>
-            Form
-          </Link>
-        </nav>
-      </header>
-    );
-  }
-}
+        <Link className="header-link" to="/about" onClick={onLinkClick}>
+          About
+        </Link>
+        <Link className="header-link" to="/form" onClick={onLinkClick}>
+          Form
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
