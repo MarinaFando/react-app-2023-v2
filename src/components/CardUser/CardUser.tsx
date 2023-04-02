aa3d7577b229dfcfd8ec2ed5f4439cf67110fe15
Marina Fando <marinafando@gmail.com>
2023-04-02 19:50:32 +0200

    refactor: card user component to functional component

--- src/components/CardUser/CardUser.tsx
+++ src/components/CardUser/CardUser.tsx
@@ -6,8 +6,7 @@ interface CardUserProps {
   user: User;
 }
 
-class CardUser extends React.Component<CardUserProps> {
-  render() {
+const CardUser = ({user<CardUserProps>}) => {
     return (
       <div className="formcard">
         <div className="formcard__left">
@@ -16,36 +15,35 @@ class CardUser extends React.Component<CardUserProps> {
         <div className="formcard__right">
           <div className="formcard__right--group">
             <h5>First Name:</h5>
-            <div>{this.props.user.firstName}</div>
+            <div>{props.user.firstName}</div>
           </div>
           <div className="formcard__right--group">
             <h5>Last Name:</h5>
-            <div>{this.props.user.lastName}</div>
+            <div>{props.user.lastName}</div>
           </div>
           <div className="formcard__right--group">
             <h5>Phone number:</h5>
-            <div>{this.props.user.phoneNumber}</div>
+            <div>{props.user.phoneNumber}</div>
           </div>
           <div className="formcard__right--group">
             <h5>Email:</h5>
-            <div>{this.props.user.email}</div>
+            <div>{props.user.email}</div>
           </div>
           <div className="formcard__right--group">
             <h5>Birth date:</h5>
-            <div>{this.props.user.birthday}</div>
+            <div>{props.user.birthday}</div>
           </div>
           <div className="formcard__right--group">
             <h5>Country:</h5>
-            <div>{this.props.user.country}</div>
+            <div>{props.user.country}</div>
           </div>
           <div className="formcard__right--group">
             <h5>Gender:</h5>
-            <div>{this.props.user.gender}</div>
+            <div>{props.user.gender}</div>
           </div>
         </div>
       </div>
     );
-  }
 }
 
 export default CardUser;
