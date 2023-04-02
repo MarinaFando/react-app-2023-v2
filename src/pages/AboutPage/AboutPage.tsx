034affff06aa9a23220afb269d76a04e51351e01
Marina Fando <marinafando@gmail.com>
2023-04-02 19:54:22 +0200

    refactor: about page to functional component

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import '../../styles/About.css';
 
-class AboutPage extends React.Component {
-  render() {
+const AboutPage = () => {
     return (
       <section className="container-about">
         <p>Nice to see you here!</p>
       </section>
     );
-  }
 }
 
 export default AboutPage;
