f811cd0ef5821aad861a325bfc39a1f4e511914a
Marina Fando <marinafando@gmail.com>
2023-04-02 19:50:57 +0200

    refactor: card search panel component to functional component

--- src/components/SearchPanel.tsx
+++ src/components/SearchPanel.tsx
@@ -6,23 +6,21 @@ interface SearchPanelProps {
   onSearchValueChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-class SearchPanel extends React.Component<SearchPanelProps> {
-  render() {
-    return (
-      <section className="block">
-        <div className="search__block">
-          <img className="search__block-icon" src="img/search.svg" />
-          <input
-            name="inputvalue"
-            type="text"
-            placeholder="Search..."
-            value={this.props.searchValue}
-            onChange={this.props.onSearchValueChange}
-          />
-        </div>
-      </section>
-    );
-  }
-}
+const SearchPanel = ({ searchValue, onSearchValueChange }: SearchPanelProps) => {
+  return (
+    <section className="block">
+      <div className="search__block">
+        <img className="search__block-icon" src="img/search.svg" />
+        <input
+          name="inputvalue"
+          type="text"
+          placeholder="Search..."
+          value={searchValue}
+          onChange={onSearchValueChange}
+        />
+      </div>
+    </section>
+  );
+};
 
 export default SearchPanel;
