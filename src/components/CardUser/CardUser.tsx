aaed2f0ec98f7bc0ae9de9ee62a947129a76689a
Marina Fando <marinafando@gmail.com>
2023-04-02 20:06:25 +0200

    refactor: carduser component

--- src/components/CardUser/CardUser.tsx
+++ src/components/CardUser/CardUser.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import '../../styles/CardUser.css';
 import { User } from '../FormsLayout/FormsLayout';
 
-interface CardUserProps {
+interface CardUser{
   user: User;
 }
 
-const CardUser = ({user<CardUserProps>}) => {
+const CardUser = ({user}: CardUser) => {
     return (
       <div className="formcard">
         <div className="formcard__left">
@@ -15,31 +15,31 @@ const CardUser = ({user<CardUserProps>}) => {
         <div className="formcard__right">
           <div className="formcard__right--group">
             <h5>First Name:</h5>
-            <div>{props.user.firstName}</div>
+            <div>{user.firstName}</div>
           </div>
           <div className="formcard__right--group">
             <h5>Last Name:</h5>
-            <div>{props.user.lastName}</div>
+            <div>{user.lastName}</div>
           </div>
           <div className="formcard__right--group">
             <h5>Phone number:</h5>
-            <div>{props.user.phoneNumber}</div>
+            <div>{user.phoneNumber}</div>
           </div>
           <div className="formcard__right--group">
             <h5>Email:</h5>
-            <div>{props.user.email}</div>
+            <div>{user.email}</div>
           </div>
           <div className="formcard__right--group">
             <h5>Birth date:</h5>
-            <div>{props.user.birthday}</div>
+            <div>{user.birthday}</div>
           </div>
           <div className="formcard__right--group">
             <h5>Country:</h5>
-            <div>{props.user.country}</div>
+            <div>{user.country}</div>
           </div>
           <div className="formcard__right--group">
             <h5>Gender:</h5>
-            <div>{props.user.gender}</div>
+            <div>{user.gender}</div>
           </div>
         </div>
       </div>
