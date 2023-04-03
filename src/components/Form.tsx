963a305ca27eaa8ce5687e2bf4c4565126e3fd5e
Marina Fando <marinafando@gmail.com>
2023-04-04 01:43:25 +0200

    refactor: rewrite form using react-hook-form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,53 +1,107 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/Form.css';
 import { FormFields } from '../FormsLayout';
+import { useForm } from 'react-hook-form';
 
 interface UserListProps {
   addToUsersList: (form: FormFields) => void;
 }
+
 const Form = ({ addToUsersList }: UserListProps) => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement & FormFields>) => {
-    event.preventDefault();
-    const form = event.currentTarget;
+  const { register, reset, handleSubmit, setValue, formState } = useForm<FormFields>({
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      email: '',
+      country: 'Switzerland',
+      gender: '',
+      photo: '',
+    },
+  });
+
+  const onSubmit = (form: FormFields) => {
     try {
       addToUsersList(form);
     } catch (error) {
       alert(error);
     }
-    form.reset();
   };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files[0];
+    const url = file ? URL.createObjectURL(file) : '';
+    setValue('photo', url);
+  };
+
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset();
+    }
+  }, [formState, reset]);
 
   return (
     <div className="form__container">
-      <form className="form" onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <div className="form__group">
           <label htmlFor="firstName">
             First Name:
-            <input type="text" name="firstName" id="firstName" required />
+            <input
+              type="text"
+              {...register('firstName', {
+                required: 'this field is required',
+              })}
+              id="firstName"
+            />
           </label>
         </div>
         <div className="form__group">
           <label htmlFor="lastName">
             Last Name:
-            <input type="text" name="lastName" id="lastName" required />
+            <input
+              type="text"
+              {...register('lastName', {
+                required: 'this field is required',
+              })}
+              id="lastName"
+            />
           </label>
         </div>
         <div className="form__group">
           <label htmlFor="phoneNumber">
             Phone number:
-            <input type="number" name="phoneNumber" id="phoneNumber" required />
+            <input
+              type="tel"
+              pattern="[0-9]{7,12}"
+              {...register('phoneNumber', {
+                required: 'this field is required',
+              })}
+              id="phoneNumber"
+            />
           </label>
         </div>
         <div className="form__group">
           <label htmlFor="email">
             Email:
-            <input type="email" name="email" id="email" required />
+            <input
+              type="email"
+              {...register('email', {
+                required: 'this field is required',
+              })}
+              id="email"
+            />
           </label>
         </div>
         <div className="form__group">
           <label htmlFor="birthday">
             Birth date:
-            <input type="date" name="birthday" id="birthday" required />
+            <input
+              type="date"
+              {...register('birthday', {
+                required: 'this field is required',
+              })}
+              id="birthday"
+            />
           </label>
         </div>
         <div className="form__group">
@@ -55,7 +109,9 @@ const Form = ({ addToUsersList }: UserListProps) => {
             Country:
             <select
               className="form__group--country"
-              name="country"
+              {...register('country', {
+                required: 'this field is required',
+              })}
               id="country"
               defaultValue="Switzerland"
             >
@@ -69,25 +125,53 @@ const Form = ({ addToUsersList }: UserListProps) => {
           <fieldset>
             <legend>Gender:</legend>
             <label htmlFor="male">
-              <input type="radio" name="gender" value="male" id="male" defaultChecked />
+              <input
+                type="radio"
+                {...register('gender', {
+                  required: 'this field is required',
+                })}
+                value="male"
+                id="male"
+                defaultChecked
+              />
               male
             </label>
             <label htmlFor="female">
-              <input type="radio" name="gender" value="female" id="female" />
+              <input
+                type="radio"
+                {...register('gender', {
+                  required: 'this field is required',
+                })}
+                value="female"
+                id="female"
+              />
               female
             </label>
             <label htmlFor="other">
-              <input type="radio" name="gender" value="other" id="other" />
+              <input
+                type="radio"
+                {...register('gender', {
+                  required: 'this field is required',
+                })}
+                value="other"
+                id="other"
+              />
               other
             </label>
           </fieldset>
         </div>
         <div className="form__group">
           <label htmlFor="photo">Photo:</label>
-          <input type="file" name="photo" />
+          <input type="file" onChange={handleFileChange} required />
         </div>
         <div className="form__group">
-          <input type="checkbox" name="agree" required id="agreeCheckbox" />
+          <input
+            type="checkbox"
+            {...register('agree', {
+              required: 'this field is required',
+            })}
+            id="agreeCheckbox"
+          />
           <label htmlFor="agree">I agree with using my personal information</label>
         </div>
         <input type="submit" value="Submit" id="submit" />
