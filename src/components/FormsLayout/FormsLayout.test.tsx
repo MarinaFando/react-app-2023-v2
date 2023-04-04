a8f24ce9e2c4227bfcf103002d3f93b5dd7a10a2
Marina Fando <marinafando@gmail.com>
2023-04-04 14:26:44 +0200

    test: correct logic

--- src/components/FormsLayout/FormsLayout.test.tsx
+++ src/components/FormsLayout/FormsLayout.test.tsx
@@ -6,16 +6,17 @@ describe('FormsLayout', () => {
   it('renders FormsLayout component', () => {
     render(<FormsLayout />);
     expect(screen.getByText(/First Name:/i)).toBeInTheDocument();
+    expect(screen.getByTestId('firstName').value).toBe('');
     expect(screen.getByText(/Last Name:/i)).toBeInTheDocument();
     expect(screen.getByText(/Phone number:/i)).toBeInTheDocument();
     expect(screen.getByText(/Email:/i)).toBeInTheDocument();
     expect(screen.getByText(/Birth date:/i)).toBeInTheDocument();
     expect(screen.getByText(/Country:/i)).toBeInTheDocument();
-    expect(screen.getByDisplayValue(/Switzerland/i)).toBeInTheDocument();
+    expect(screen.getByTestId('country').value).toBe('');
     expect(screen.getByText(/Gender:/i)).toBeInTheDocument();
-    expect(screen.getByDisplayValue(/female/i)).toBeInTheDocument();
+    expect(screen.getByText(/female/i)).toBeInTheDocument();
     expect(screen.getByText(/Photo:/i)).toBeInTheDocument();
     expect(screen.getByRole('checkbox')).toBeInTheDocument();
-    expect(screen.getByDisplayValue(/Submit/i)).toBeInTheDocument();
+    expect(screen.getByText(/Submit/i)).toBeInTheDocument();
   });
 });
